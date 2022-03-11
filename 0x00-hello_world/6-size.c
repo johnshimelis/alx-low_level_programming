51be4ad6bf89dea4492020f05776ebbc93f23dcf
johnshimelis <johnshimelis09@gmail.com>
2022-03-10 18:31:53 -0800

    Create 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#inclde <stdio.h>
+/**
+*main - entry point
+*My hello, printf task
+*Return: 0 after program execution 
+*/
+int main(void)
+{
+  printf("Size of a char: %d byte(s)\n"), Sizeof(char));
+  printf("Size of an int: %d byte(s)\n"), Sizeof(int));
+  printf("Size of a long int: %d byte(s)\n"), Sizeof(long int));
+  printf("Size of a long long int: %d byte(s)\n"), Sizeof(long long int));
+  printf("Size of a float: %d byte(s)\n"), Sizeof(float));
+  return(0);
+}
