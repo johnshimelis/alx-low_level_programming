4239214077a7092fb7a5148062da51a97239e387
johnshimelis <johnshimelis09@gmail.com>
2022-03-10 18:26:20 -0800

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#inclde <stdio.h>
+/**
+*main - entry point
+*My hello, printf task
+*Return: 0 after program execution 
+*/
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art, \n");
+  return(0);
+}
