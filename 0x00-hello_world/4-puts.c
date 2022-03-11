e4465b8a213349597948167b8cc19b9b38ca6a0b
johnshimelis <johnshimelis09@gmail.com>
2022-03-10 18:22:44 -0800

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#inclde <stdio.h>
+/**
+*main - entry point
+*
+*Return: 0 after printing the function
+*/
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return(0);
+}
