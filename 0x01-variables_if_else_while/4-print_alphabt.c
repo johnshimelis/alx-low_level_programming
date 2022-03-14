f8e60e382c50b1f801a414d1cab22c7f9c72438b
johnshimelis <johnshimelis09@gmail.com>
2022-03-14 12:55:51 +0300

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet except 'e' and 'q'
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
