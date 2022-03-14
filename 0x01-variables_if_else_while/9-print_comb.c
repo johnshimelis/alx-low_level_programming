b813bc608ae7b330dd6b4080057d99de9b2fb7ab
johnshimelis <johnshimelis09@gmail.com>
2022-03-14 12:59:15 +0300

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digits separated by comma and space
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		putchar(ch);
+		if (ch != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
