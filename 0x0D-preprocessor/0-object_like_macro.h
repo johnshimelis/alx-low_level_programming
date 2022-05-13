600e4600b76a9c49ed67b3cb63ff66d9bf69366d
johnshimelis <johnshimelis09@gmail.com>
2022-05-13 02:31:02 -0700

    Add files via upload

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,3 @@
-#include <stdio.h>
+#ifndef SIZE
 #define SIZE 1024
-
-int main(void)
-{
-    int s;
-
-    s = 98 + SIZE;
-    printf("%d\n", s);
-    printf("%d\n", SIZE);
-    return (0);
-}
+#endif
